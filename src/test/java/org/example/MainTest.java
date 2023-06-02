abb071e63dddb1e76e590e161626a3d0afca21f4
Sahed Wahedi <sahed.w@hotmail.de>
2023-06-02 11:00:43 +0200

    write the test for validating the length of the password

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    void yieldTrue_whenCalledWith12345678() {
+
+        boolean actual = Main.validateLength();
+
+        assertTrue(actual);
+    }
 
 }
