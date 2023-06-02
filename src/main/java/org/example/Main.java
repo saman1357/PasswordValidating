6caa99efa0eea32dcaca81bea785278504961d79
Sahed Wahedi <sahed.w@hotmail.de>
2023-06-02 11:10:29 +0200

    write validateLength method in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+
+    String pw = "12345678";
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public static boolean validateLength(String password) {
+        if (password.length() >= 8) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
