621cde990d7a27fb3bf835570a6285b1e9928277
Sahed Wahedi <sahed.w@hotmail.de>
2023-06-02 14:46:53 +0200

    import scanner and create scanner object

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
 
-    String pw = "12345678";
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
